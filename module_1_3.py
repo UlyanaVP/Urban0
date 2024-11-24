ad05df207c9a3009eaa2080866302748809c5dee
UlyanaVP <katya.ayushieva.06@gmail.com>
2024-11-24 23:32:08 +0900

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,8 @@
 name='Ulyana'
 age=15
-new_age='16'
+new_age=age+1
 is_student='True'
 print('Name: ' + name)
 print('Age: ' , age)
-print('New Age: ' + new_age)
+print('New Age: ' , new_age)
 print('Is Student: ' + is_student)
